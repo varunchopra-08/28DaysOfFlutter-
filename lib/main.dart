3e1318c1ae508a4de3ecab88bc3a1d6007272f14
Varun <varunchopra360@gmail.com>
2024-01-17 17:46:44 +0530

    day 5 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chapter_1/pages/home_page.dart';
 import 'package:chapter_1/pages/login_page.dart';
+import 'package:chapter_1/pages/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -25,8 +26,8 @@ class MyApp extends StatelessWidget {
         
         routes: {
           "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
         });
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import "package:chapter_1/pages/utils/routes.dart";
 import "package:flutter/material.dart";
 
 class LoginPage extends StatelessWidget {
@@ -7,52 +8,54 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_img.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
-              child: Column(children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: ("Enter Username"),
-                    labelText: ("Username"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ("Enter Username"),
+                      labelText: ("Username"),
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: ("Enter Password"),
-                    labelText: ("Password"),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: ("Enter Password"),
+                      labelText: ("Password"),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('hi varun');
-                  },
-                  child: Text("Sign In"),
-                  style: TextButton.styleFrom(),
-                )
-              ]),
-            ),
-          ],
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: Text("Sign In"),
+                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  )
+                ]),
+              ),
+            ],
+          ),
         ));
   }
 }
